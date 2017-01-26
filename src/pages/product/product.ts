dc7b961cd218386e25b5d3fadbaeb771266b2137
Javier Bono Parra <jotabono92@gmail.com>
2017-01-26 13:39:29 +0100

    Catalog download added

--- src/pages/product/product.ts
+++ src/pages/product/product.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
-import { NavController,NavParams } from 'ionic-angular';
+import { NavController, NavParams, Platform } from 'ionic-angular';
 import {Http} from '@angular/http';
 import { LoadingController } from 'ionic-angular';
 import { HomePage } from '../home/home';
 import { ContactPage } from '../contact/contact';
-import { CatalogPage } from '../catalog/catalog';
+import { Transfer } from 'ionic-native';
+declare var cordova: any;
+declare var window: any;
+
 @Component({
   selector: 'page-product',
   templateUrl: 'product.html'
@@ -14,43 +17,56 @@ export class ProductPage {
   products = [];
   dataTaxonomyUrl: string;
   dataProductUrl: string;
-  parent_name : string;
-  name : string;
-  subtitle : string;
-  description : HTMLElement;
-  image:string;
-  constructor(public navCtrl: NavController, private http: Http,public params:NavParams,public loadingCtrl: LoadingController) {
+  parent_name: string;
+  name: string;
+  subtitle: string;
+  description: HTMLElement;
+  image: string;
+  constructor(public navCtrl: NavController, private http: Http, public params: NavParams, public loadingCtrl: LoadingController, private platform: Platform) {
     let loader = this.loadingCtrl.create({
       content: "Cargando...",
       duration: 1500
     });
     loader.present();
-    this.dataTaxonomyUrl = 'http://dosilet.deideasmarketing.solutions/wp-json/wp/v2/get_taxonomy_data_bycategory?idcategory='+params.get("idCategory");
-    this.dataProductUrl = 'http://dosilet.deideasmarketing.solutions/wp-json/wp/v2/get_products_bycategory?idcategory='+params.get("idCategory");
+    this.dataTaxonomyUrl = 'http://dosilet.deideasmarketing.solutions/wp-json/wp/v2/get_taxonomy_data_bycategory?idcategory=' + params.get("idCategory");
+    this.dataProductUrl = 'http://dosilet.deideasmarketing.solutions/wp-json/wp/v2/get_products_bycategory?idcategory=' + params.get("idCategory");
     this.http.get(this.dataTaxonomyUrl)
       .map(res => res.json())
       .subscribe(data => {
         this.taxonomies[0] = data[0];
-        this.parent_name = (this.taxonomies[0].parent_name != null)?this.taxonomies[0].parent_name:"" ;
+        this.parent_name = (this.taxonomies[0].parent_name != null) ? this.taxonomies[0].parent_name : "";
         this.name = this.taxonomies[0].name;
         this.subtitle = this.taxonomies[0].subtitle;
         this.description = this.taxonomies[0].description;
       });
     this.http.get(this.dataProductUrl)
-        .map(res => res.json())
-        .subscribe(data => {
-          for (var i = 0; i < data.length; i++) {
-             console.log(data[i].image.url);
-              this.image = (data[i].image!= false && data[i].image.sizes.medium != "undefined") ? data[i].image.sizes.medium: "";
-              this.products.push({ id: data[i].idproduct , image:this.image, name:data[i].product.post_title });
-          }
-          console.log(this.products);
-        });
+      .map(res => res.json())
+      .subscribe(data => {
+        for (var i = 0; i < data.length; i++) {
+          console.log(data[i].image.url);
+          this.image = (data[i].image != false && data[i].image.sizes.medium != "undefined") ? data[i].image.sizes.medium : "";
+          this.products.push({ id: data[i].idproduct, image: this.image, name: data[i].product.post_title });
+        }
+        console.log(this.products);
+      });
+  }
+  downloadFile() {
+    const fileTransfer = new Transfer();
+    let url = 'http://dosilet.deideasmarketing.solutions/wp-content/uploads/2017/01/Diagrama-2-1.pdf';
+
+    fileTransfer.download(url, cordova.file.dataDirectory + 'Diagrama-2-1.pdf').then((entry) => {
+      console.log('download complete: ' + entry.toURL());
+      this.platform.ready().then(() => {
+        window.plugins.toast.show("Catálogo descargado correctamente.", "short", "center");
+      });
+    }, (error) => {
+      console.log('Errorrr');
+    });
   }
   openHome() {
     this.navCtrl.setRoot(HomePage);
   }
-  goBack(){
+  goBack() {
     this.navCtrl.pop();
   }
   goContact() {
